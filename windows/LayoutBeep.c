c8bcdd0e3c0edf4857d7f5ec6a62854c2ff5b683
Vlad-MyHomeLab <mygithub2@cympak.biz>
2026-09-14 00:01:00 +0300

    Добавить исходник Windows-приложения

---
+++ windows/LayoutBeep.c
@@ -0,0 +1,7 @@
+/* LayoutBeep: нативное Win32-приложение.
+   Исходник разделён на последовательные include-фрагменты только для удобства хранения. */
+#include "LayoutBeep_01.inc"
+#include "LayoutBeep_02.inc"
+#include "LayoutBeep_03.inc"
+#include "LayoutBeep_04.inc"
+#include "LayoutBeep_05.inc"
